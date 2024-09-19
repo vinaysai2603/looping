7fe940d9de407fd869199fb6ecd772a2626d20ea
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:51:44 -0700

    Update lucas.py

--- lucas.py
+++ lucas.py
@@ -10,3 +10,16 @@ Sample Input:
 Sample Output:
 0 0 1 1 2
 '''
+n=int(input())
+a=0
+b=0
+c=1
+print(a,end=’ ‘)
+print(b,end=’ ‘)
+print(c,end=’ ‘)
+for i in range (4,n+1):
+	d=a+b+c
+	a=b
+	b=c
+	c=d
+	print(d,end=’ ‘)
