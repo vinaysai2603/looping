3084adad72890f986f92497941dd51009d598de4
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:43:52 -0700

    Create fact.py

---
+++ fact.py
@@ -0,0 +1,12 @@
+'''
+Write a program to determine whether 'n' is a factorial number or not. Factorial of a number is the product of all positive numbers from 1 to 'n'.
+Input format:
+The input containing an integer 'n' which denotes the given number.
+Output format:
+If the given number is factorial, print "Yes". Otherwise, print "No".
+Refer the sample output for formatting.
+Sample Input:
+6
+Sample Output:
+Yes
+''' 
