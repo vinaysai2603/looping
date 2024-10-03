f740c08f7c8576fac05c10f648304d4606383052
vinaysai2603 <vinaysai0326@gmail.com>
2024-10-03 01:19:35 -0700

    Update trendynum.py

--- trendynum.py
+++ trendynum.py
@@ -10,3 +10,16 @@ Sample Input:
 Sample Output:
 Trendy Number
 '''
+n=int(input())
+if(n>=100 and n<=999):
+    a=n//10
+    b=a%10
+    if(b%3==0):
+        print("Trendy Number")
+    else:
+        print("Not a Trendy number")
+else:
+    print("Not a Trendy Number")
+
+
+
